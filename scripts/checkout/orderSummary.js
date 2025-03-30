f16c2225e6c639a9b71017c1d10bb7c78ef1e86a
PedroHenriqueJS <pedrohjs2013@gmail.com>
2025-03-30 19:32:32 -0300

    criado sumario interativo

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,7 +3,7 @@ import { products, getProduct } from '../../data/products.js'
 import { formatCurrency } from '../utils/money.js'
 import dayjs  from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
-
+import { renderPaymentSummary } from './paymentSummary.js'
 const orderSummary = document.querySelector('.js-order-summary')
 
 
@@ -140,6 +140,7 @@ export function renderOrderSummary(){
             removeFromCart(productId)
             const container = document.querySelector(`.js-cart-item-container-${productId}`)
             container.remove()
+            renderPaymentSummary()
             calculateCartQuantity()
         })
     })
@@ -149,6 +150,7 @@ export function renderOrderSummary(){
             const {productId, deliveryOptionId} = element.dataset
             updateDeliveryOption(productId, deliveryOptionId)
             renderOrderSummary()
+            renderPaymentSummary()
         })
     })
 }
