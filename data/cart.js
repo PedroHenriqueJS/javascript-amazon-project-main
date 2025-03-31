7846701dd679d5f00d235c3fc93070a33470c344
PedroHenriqueJS <pedrohjs2013@gmail.com>
2025-03-31 16:30:14 -0300

    renomeado framework de teste jasmine para testes

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,27 @@
 // MODEL
 
+export let cart;
 
-export let cart = JSON.parse(localStorage.getItem('cart')) || [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }]
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1',
+      },
+      {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2',
+      },
+    ];
+  }
+}
 
 
 function saveStorage(){
@@ -33,26 +45,27 @@ export function calculateCartQuantity() {
   if (countQuantityElement) {
     countQuantityElement.textContent = `${cartQty} Items`;
   }
+
 }
-export function addToCart(productId){
-  const qtySelector = document.querySelector(`.js-quantity-selector-${productId}`).value
+export function addToCart(productId) {
   let matchingItem
-  
+
   cart.forEach((cartItem) => {
-    if(productId === cartItem.productId){
+    if (productId === cartItem.productId) {
       matchingItem = cartItem
     }
   })
-  
-  if(matchingItem){
-    matchingItem.quantity += Number(qtySelector)
+
+  if (matchingItem) {
+    matchingItem.quantity += 1
   } else {
-    cart.push({ 
-      productId, 
-      quantity: +qtySelector ,
+    cart.push({
+      productId: productId,
+      quantity: 1,
       deliveryOptionId: '1'
     })
   }
+
   saveStorage()
 }
 
