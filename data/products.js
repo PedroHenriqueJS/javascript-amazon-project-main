933cbc310a47db36c50232ad34cce5960244b305
PedroHenriqueJS <pedrohjs2013@gmail.com>
2025-04-01 01:52:26 -0300

    convertido products em classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,6 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+
 export function getProduct(productId) {
   let matchingProduct
     
@@ -11,6 +14,32 @@ export function getProduct(productId) {
 }
 
 // MODEL
+
+
+class Product {
+  id
+  image
+  name
+  rating
+  priceCents
+
+  constructor(productDetails) {
+    this.id = productDetails.id
+    this.image = productDetails.image
+    this.name = productDetails.name
+    this.rating = productDetails.rating
+    this.priceCents = productDetails.priceCents
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`
+  }
+
+}
 
 export const products = [
   {
@@ -698,4 +727,6 @@ export const products = [
       "accessories",
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails)
+});
